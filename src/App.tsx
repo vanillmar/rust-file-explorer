be47a3cb58396f41e4d772e5a5eef9d214ac439f
Vanilson Marcos <vanilson.marcos@unipiaget-angola.org>
2025-05-06 11:07:41 +0100

    feat: integrate Tailwind CSS for styling and remove legacy CSS styles
    - Replaced existing CSS styles in App.css with Tailwind CSS directives.
    - Updated App.tsx to utilize Tailwind CSS classes for styling.
    - Added Tailwind CSS plugin to Vite configuration.
    - Created a new Tailwind configuration file (tailwind.config.js).

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
@@ -14,21 +13,7 @@ function App() {
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
+      <h1 className="text-3xl font-bold underline">Welcome to Tauri + React</h1>
       <form
         className="row"
         onSubmit={(e) => {
